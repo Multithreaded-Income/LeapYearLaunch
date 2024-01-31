c9892f95f781f5bf83ea89e48bccf9a78ae0589b
Kevin Griffin <kevin@kevgriffin.com>
2024-01-31 17:00:10 -0600

    wip

---
+++ src/components/FontAwesomeIcon.vue
@@ -0,0 +1,20 @@
+<template>
+  <FontAwesomeIcon :icon="iconName" />
+</template>
+
+<script setup lang="ts">
+import { defineProps, computed } from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTwitter, faYoutube, faTiktok, faInstagram, faDiscord } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faTwitter, faYoutube, faTiktok, faInstagram, faDiscord);
+
+const props = defineProps({
+  icon: String
+});
+
+const iconName = computed(() => {
+  return props.icon;
+});
+</script>
